4f2dbd22fcf45930be4209c0fdee2488d064bec8
raj1rana <rajendar1rana@gmail.com>
2020-02-21 14:47:36 +0530

    adding shop page

--- src/App.js
+++ src/App.js
@@ -2,19 +2,14 @@ import React from 'react';
 import {Route} from 'react-router-dom'
 import './App.css';
 import HomePage from './pages/homepage/homepage-component'
-
+import ShopPage from './pages/shop/shop-components.jsx'
   
-const HatsPage = ()=>(
-  
-  <div>
-    <h1>Hats page</h1>
-  </div>
-);
 
 function App() {
   return (
     <div >
      <Route exact path='/' component={HomePage} />
+     <Route exact path='/shop' component={ShopPage} />
     </div>
   );
 }
---
+++ src/components/collection-preview/collection-preview-component.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-expressions */
+import React from 'react'
+import CollectionItem  from '../collection-items/collection-items'
+import './collection-preview-component.scss'
+const CollectionPreview = ({title, items}) => {
+    return(<div className ='collection-preview'>
+    <h1 className= 'title'>{title.toUpperCase()}</h1>
+    <div className='preview'>
+        {items.filter((item, index)=> index < 4).map(({id, ...itemProps}) => (<CollectionItem key={id}{...itemProps}/>))}
+    </div>
+</div>)
+};
+
+export default CollectionPreview
